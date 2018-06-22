e07cc08651ec224401a8f97ef262f78ca1ee359c
Sachin <sachin.kumar@goodworklabs.com>
2018-06-22 18:26:16 +0530

    Issue #4 fix: question unit interface and code refractor

--- editor/controllers/horizontal_controller.js
+++ editor/controllers/horizontal_controller.js
@@ -1,10 +1,14 @@
-/**
+/*
  * Plugin to create MCQ question
  * @class org.ekstep.questionunitmcq:mcqQuestionFormController
  * Jagadish P<jagadish.pujari@tarento.com>
  */
-
 angular.module('ftbApp', []).controller('ftbQuestionFormController', ['$scope', '$rootScope', function($scope, $rootScope) { // eslint-disable-line no-unused-vars
+  var addAllMedia, questionInput;
+  //get ftb plugin manifest
+  $scope.ftbPluginInstance = org.ekstep.pluginframework.pluginManager.getPluginManifest("org.ekstep.questionunit.ftb");
+  //get keyboard plugin manifest
+  $scope.keyboardPluginInstance = org.ekstep.pluginframework.pluginManager.getPluginManifest("org.ekstep.keyboard");
   $scope.keyboardConfig = {
     keyboardType: 'Device',
     customKeys: []
@@ -29,31 +33,31 @@ angular.module('ftbApp', []).controller('ftbQuestionFormController', ['$scope', 
     answer: [],
     media: []
   };
-  var eraserIcon = {
+  //add media in question object media array
+  addAllMedia = [{
     id: "org.ekstep.keyboard.eras_icon",
-    src: ecEditor.resolvePluginResource("org.ekstep.keyboard", "1.0", 'renderer/assets/eras_icon.png'),
+    src: ecEditor.resolvePluginResource($scope.keyboardPluginInstance.id, $scope.keyboardPluginInstance.ver, 'renderer/assets/eras_icon.png'),
     assetId: "org.ekstep.keyboard.eras_icon",
     type: "image",
     preload: true
-  };
-  $scope.ftbFormData.media.push(eraserIcon);
-  var languageIcon = {
+  }, {
     id: "org.ekstep.keyboard.language_icon",
-    src: ecEditor.resolvePluginResource("org.ekstep.keyboard", "1.0", 'renderer/assets/language_icon.png'),
+    src: ecEditor.resolvePluginResource($scope.keyboardPluginInstance.id, $scope.keyboardPluginInstance.ver, 'renderer/assets/language_icon.png'),
     assetId: "org.ekstep.keyboard.language_icon",
     type: "image",
     preload: true
-  };
-  $scope.ftbFormData.media.push(languageIcon);
-  var hideKeyboardIcon = {
+  }, {
     id: "org.ekstep.keyboard.hide_keyboard",
-    src: ecEditor.resolvePluginResource("org.ekstep.keyboard", "1.0", 'renderer/assets/keyboard.svg'),
+    src: ecEditor.resolvePluginResource($scope.keyboardPluginInstance.id, $scope.keyboardPluginInstance.ver, 'renderer/assets/keyboard.svg'),
     assetId: "org.ekstep.keyboard.hide_keyboard",
     type: "image",
     preload: true
-  };
-  $scope.ftbFormData.media.push(hideKeyboardIcon);
-  var questionInput = CKEDITOR.replace('ftbQuestion', { // eslint-disable-line no-undef
+  }];
+  //push media into ftbform media
+  addAllMedia.forEach(function(obj) {
+    $scope.ftbFormData.media.push(obj);
+  })
+  questionInput = CKEDITOR.replace('ftbQuestion', { // eslint-disable-line no-undef
     customConfig: CKEDITOR.basePath + "config.js", // eslint-disable-line no-undef
     skin: 'moono-lisa,' + CKEDITOR.basePath + "skins/moono-lisa/", // eslint-disable-line no-undef
     contentsCss: CKEDITOR.basePath + "contents.css" // eslint-disable-line no-undef
@@ -62,66 +66,109 @@ angular.module('ftbApp', []).controller('ftbQuestionFormController', ['$scope', 
     $scope.ftbFormData.question.text = this.getData();
   });
   questionInput.on('focus', function() {
-    $scope.generateTelemetry({ type: 'TOUCH', id: 'input', target: { id: 'questionunit-ftb-question', ver: '', type: 'input' } })
-  });
-  $scope.init = function() {
-    $('.menu .item').tab();
-    $('.ui.dropdown').dropdown({
-      useLabels: false
-    });
-    if (!ecEditor._.isUndefined($scope.questionEditData)) {
-      var data = $scope.questionEditData.data;
-      $scope.ftbFormData.question = data.question;
-      $scope.keyboardConfig = data.question.keyboardConfig;
-    }
-    $scope.$parent.$on('question:form:val', function() { // eslint-disable-line no-unused-vars
-      var regexForAns = /(?:^|)\[\[(.*?)(?:\]\]|$)/g;
-      $scope.ftbFormData.answer = $scope.getMatches($scope.ftbFormData.question.text, regexForAns, 1).map(function(a) {
-        return a.toLowerCase().trim();
-      });
-      if ($scope.formValidation()) {
-        $scope.$emit('question:form:valid', $scope.ftbFormData);
-      } else {
-        $scope.$emit('question:form:inValid', $scope.ftbFormData);
+    $scope.generateTelemetry({
+      type: 'TOUCH',
+      id: 'input',
+      target: {
+        id: 'questionunit-ftb-question',
+        ver: '',
+        type: 'input'
       }
     })
+  });
+  $scope.init = function() {
+    /**
+   * editor:questionunit.ftb:call form validation.
+   * @event org.ekstep.questionunit.ftb:validateform
+   * @memberof org.ekstep.questionunit.ftb.horizontal_controller
+   */
+    ecEditor.addEventListener('org.ekstep.questionunit.ftb:validateform', function(event, callback) {
+      var validationRes = $scope.formValidation();
+      callback(validationRes.isValid, validationRes.formData);
+    }, $scope);
+    /**
+   * editor:questionunit.ftb:call form edit the question.
+   * @event org.ekstep.questionunit.ftb:editquestion
+   * @memberof org.ekstep.questionunit.ftb.horizontal_controller
+   */
+    ecEditor.addEventListener('org.ekstep.questionunit.ftb:editquestion', $scope.editFtbQuestion, $scope);
+    ecEditor.dispatchEvent("org.ekstep.questionunit:compiled");
   }
-
-  $scope.getMatches = function(string, regex, index) {
-    index || (index = 1); // default to the first capturing group
-    var matches = [];
-    var match;
+  /**
+   * for edit flow
+   * @memberof org.ekstep.questionunit.ftb.horizontal_controller
+   * @param {event} event data.
+   * @param {question} data data.
+   */
+  $scope.editFtbQuestion = function(event, data) {
+    var qdata = data.data;
+    $scope.ftbFormData.question = qdata.question;
+    $scope.keyboardConfig = qdata.question.keyboardConfig;
+    $scope.$safeApply();
+  }
+   /**
+   * create answer array for ftb blank
+   * @memberof org.ekstep.questionunit.ftb.horizontal_controller
+   */
+  $scope.createAnswerArray = function() {
+    var regexForAns = /(?:^|)\[\[(.*?)(?:\]\]|$)/g;
+    $scope.ftbFormData.answer = $scope.splitAnswer($scope.ftbFormData.question.text, regexForAns, 1).map(function(a) {
+      return a.toLowerCase().trim();
+    });
+  }
+   /**
+   * split answer into question text
+   * @memberof org.ekstep.questionunit.ftb.horizontal_controller
+   * @returns {String} question data.
+   * @param {string} string data.
+   * @param {regex} regex expression.
+   * @param {index} index value.
+   */
+  $scope.splitAnswer = function(string, regex, index) {
+    var matches = [],
+      match;
     while (match = regex.exec(string)) { // eslint-disable-line no-cond-assign
       matches.push(match[index]);
     }
     return matches;
   }
+  /**
+   * check form validation
+   * @memberof org.ekstep.questionunit.ftb.horizontal_controller
+   * @returns {Object} question data.
+   */
   $scope.formValidation = function() {
+    var ftbFormQuestionText, formValid, formConfig = {};
     $scope.submitted = true;
-    var formValid = $scope.ftbForm.$valid && /\[\[.*?\]\]/g.test($scope.ftbFormData.question.text);
+    ftbFormQuestionText = $scope.ftbFormData.question.text;
+    formValid = (ftbFormQuestionText.length > 0) && /\[\[.*?\]\]/g.test(ftbFormQuestionText);
     if (formValid) {
-      return true;
+      $scope.createAnswerArray();
+      formConfig.isValid = true;
+      formConfig.formData = $scope.ftbFormData;
     } else {
-      $scope.ftbForm.ftbQuestion.$valid = false;
-      return false;
+      formConfig.isValid = false;
+      formConfig.formData = $scope.ftbFormData;
     }
+    return formConfig;
   }
   $scope.generateTelemetry = function(data, event) { // eslint-disable-line no-unused-vars
-    if (data) ecEditor.getService('telemetry').interact({
-      "type": data.type,
-      "id": data.id,
-      "pageid": 'question-creation-ftb-form',
-      "target": {
-        "id": data.target.id,
-        "ver": data.target.ver,
-        "type": data.target.type
-      },
-      "plugin": {
-        "id": "org.ekstep.questionunit.ftb",
-        "ver": "1.0"
-      }
-    })
+    if (data) {
+      ecEditor.getService('telemetry').interact({
+        "type": data.type || "TOUCH",
+        "id": data.id || "input",
+        "pageid": 'question-creation-ftb-form',
+        "target": {
+          "id": data.target.id || "",
+          "ver": data.target.ver || "",
+          "type": data.target.type || ""
+        },
+        "plugin": {
+          "id": $scope.ftbPluginInstance.id,
+          "ver": $scope.ftbPluginInstance.ver
+        }
+      })
+    }
   }
-
 }]);
 //# sourceURL=horizontalFTB.js
